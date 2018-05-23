6821fb99588cdb8b050dc578ccc058a000888225
Michał Brusiło <mibru@interia.pl>
2018-05-23 09:41:44 +0200

    Update Card.js

--- js/Card.js
+++ js/Card.js
@@ -4,15 +4,15 @@ function Card(id, name) {
 	
 	this.id = id;
 	this.name = name || 'No name given';
-	this.$element = createCard();
+	this.element = createCard();
 	
 	function createCard() {
-		 var $card = $('<li>').addClass('card');
-		 var $cardDescription = $('<p>').addClass('card-description').text(self.name);
+		 var card = $('<li>').addClass('card');
+		 var cardDescription = $('<p>').addClass('card-description').text(self.name);
 		// var $cardDeleteButton = $('<button>').addClass('card-delete btn');
-		 var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+		 var cardDelete = $('<button>').addClass('btn-delete').text('x');
 		
-		 $cardDelete.click(function(){
+		 cardDelete.click(function(){
 			 self.removeCard();
 		 });
 		 
